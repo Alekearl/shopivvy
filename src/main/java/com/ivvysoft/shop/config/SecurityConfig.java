e101bcb3d2cad8211d925db53c97bb3614e56f0d
Alekearl <alexandrhurko@gmail.com>
2021-03-17 23:57:24 +0200

    some corrections

--- src/main/java/com/ivvysoft/shop/config/SecurityConfig.java
+++ src/main/java/com/ivvysoft/shop/config/SecurityConfig.java
@@ -27,7 +27,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers(HttpMethod.POST,"/register").permitAll()
+                .antMatchers(HttpMethod.POST, "/register").permitAll()
+                .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .permitAll()
--- src/main/java/com/ivvysoft/shop/controller/ShoppingCartController.java
+++ src/main/java/com/ivvysoft/shop/controller/ShoppingCartController.java
@@ -1,15 +1,18 @@
 package com.ivvysoft.shop.controller;
 
+import com.ivvysoft.shop.model.Client;
 import com.ivvysoft.shop.model.Product;
 import com.ivvysoft.shop.model.ShoppingCart;
+import com.ivvysoft.shop.service.ClientService;
 import com.ivvysoft.shop.service.ProductService;
 import com.ivvysoft.shop.service.ShoppingCartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,30 +22,38 @@ import org.springframework.web.bind.annotation.RestController;
 public class ShoppingCartController {
     private final ShoppingCartService shoppingCartService;
     private final ProductService productService;
+    private final ClientService clientService;
 
     @Autowired
     public ShoppingCartController(ShoppingCartService shoppingCartService,
-                                  ProductService productService) {
+                                  ProductService productService, ClientService clientService) {
         this.shoppingCartService = shoppingCartService;
         this.productService = productService;
+        this.clientService = clientService;
     }
 
-    @GetMapping("/{cartId}")
-    public ResponseEntity<ShoppingCart> addProductToCart(@PathVariable Long cartId,
+    @PostMapping("/add")
+    public void addProductToCart(Authentication authentication,
                                            @RequestParam Long productId,
                                            @RequestParam Long quantity) {
-        ShoppingCart shoppingCart = shoppingCartService.getShoppingCartById(cartId);
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String name = userDetails.getUsername();
+        Client client = clientService.findByEmail(name).get();
+        ShoppingCart shoppingCart = shoppingCartService.getShoppingCartById(client.getId());
         Product product = productService.getProductById(productId);
         shoppingCartService.addProductToShoppingCart(shoppingCart, product, quantity);
-        return new ResponseEntity<>(shoppingCartService.getShoppingCartById(cartId), HttpStatus.OK);
     }
 
-    @DeleteMapping("/{cartId}")
-    public ResponseEntity<ShoppingCart> removeProductFromCart(@PathVariable Long cartId,
+    @DeleteMapping("/remove")
+    public ResponseEntity<ShoppingCart> removeProductFromCart(Authentication authentication,
                                       @RequestParam Long productId) {
-        ShoppingCart shoppingCart = shoppingCartService.getShoppingCartById(cartId);
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String name = userDetails.getUsername();
+        Client client = clientService.findByEmail(name).get();
+        ShoppingCart shoppingCart = shoppingCartService.getShoppingCartById(client.getId());
         Product product = productService.getProductById(productId);
         shoppingCartService.removeProductFromShoppingCart(shoppingCart, product);
-        return new ResponseEntity<>(shoppingCartService.getShoppingCartById(cartId), HttpStatus.OK);
+        return new ResponseEntity<>(shoppingCartService.getShoppingCartById(client.getId()),
+                HttpStatus.OK);
     }
 }
